8b402c396a2dd6bac6f1e1ffdac8258ae4c2a043
Lucas Krantz <lucas@gerlesborg.se>
2024-06-16 01:23:46 +0200

    Cleanup

--- Webscraper.py
+++ Webscraper.py
@@ -1,16 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 URL = "https://jus.se/collections/all-menswear/men"
 page = requests.get(URL)
 
+
 soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find(id="PageContainer")
-#print(results.prettify()) 
-
-#job_locations = results.find_all(class_="location")
-#for job_location in job_locations:
-#    print(job_location.text.strip(), end="\n"*2)
 elements = results.find_all("div", class_="grid-view-item product-card")
 for element in elements:
     title_element = element.find("a", class_="grid-view-item__link grid-view-item__image-container full-width-link")
@@ -22,4 +19,5 @@ for element in elements:
     print(f"Link: https://jus.se{link_url}\n")
     
 print(len(elements))
+
     
