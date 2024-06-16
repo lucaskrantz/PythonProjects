0e04f72e97c943dd258f4500c31e9434e59d191f
Lucas Krantz <lucas@gerlesborg.se>
2024-06-16 11:02:29 +0200

    Parallel scraping of title, price, link and description

--- Webscraper.py
+++ Webscraper.py
@@ -1,23 +1,50 @@
+import concurrent.futures
 import requests
 from bs4 import BeautifulSoup
-
 
 URL = "https://jus.se/collections/all-menswear/men"
 page = requests.get(URL)
 
-
 soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find(id="PageContainer")
 elements = results.find_all("div", class_="grid-view-item product-card")
-for element in elements:
+
+def get_product_data(url):
+    """This method will fetch data for a single product."""
+    print(f"Running {url}")
+    product_page = requests.get(url)
+    product_soup = BeautifulSoup(product_page.content, "html.parser")
+    desc_tag = product_soup.find('div', class_='product-single__description rte')
+    if desc_tag:
+        desc_text = desc_tag.text.strip()
+    else:
+        desc_text = None
+    
+    return desc_text
+
+def scrape_element(element):
     title_element = element.find("a", class_="grid-view-item__link grid-view-item__image-container full-width-link")
     price_element = element.find("span", class_="price-item price-item--regular")
     links = element.find("a", class_="grid-view-item__link grid-view-item__image-container full-width-link")
     link_url = links["href"]
-    print(title_element.text.strip())
-    print(price_element.text.strip())
-    print(f"Link: https://jus.se{link_url}\n")
+
+    prod_desc = get_product_data(f"https://jus.se{link_url}")
+    
+    return {
+        "title": title_element.text.strip(),
+        "price": price_element.text.strip(),
+        "link": f"https://jus.se{link_url}",
+        "description": prod_desc
+    }
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    results = list(executor.map(scrape_element, elements))
+
+for res in results:
+    print(res["title"])
+    print(res["price"])
+    print(f"Link: {res['link']}")
+    print(res["description"])
+    print()
     
 print(len(elements))
-
-    
